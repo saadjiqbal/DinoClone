a4f71ec879e786db3d5d59218d2dcb578102f2ba
nashtonash <41835529+nashtonash@users.noreply.github.com>
2024-07-13 13:15:35 -0400

    added frames for jump animation
    as well as a node with a script attached for leaving notes in res://

---
+++ note_node.gd
@@ -0,0 +1,8 @@
+# If you don't feel like worrying about this part, you can just use the one jump
+# frame as a placeholder and after I close out the visual-assets branch, I can 
+# handle tuning the animations.
+
+# For the jump animation, you can use frame 0 from the crouch folder as the first 
+# frame, followed by frames 0 and 1 from the jump folder. It may be necessary to have a 
+# separate animation for the windup to the crouch and another one for the 
+# sustained part of the jump.
